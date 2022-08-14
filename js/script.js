647f7f523704a3d418e953e58bde77e9971397da
Abdukarimxonov <abdukarimxonov2004@gmail.com>
2022-08-14 11:45:23 +0500

    gg

--- js/script.js
+++ js/script.js
@@ -214,3 +214,17 @@ const senseisSwiper = new Swiper('.sensei .swiper', {
       },
    },
 });
+
+$(document).ready(function () {
+   $("a").on('click', function (event) {
+      if (this.hash !== "") {
+         event.preventDefault();
+         var hash = this.hash;
+         $('html, body').animate({
+            scrollTop: $(hash).offset().top
+         }, 800, function () {
+            window.location.hash = hash;
+         });
+      }
+   });
+});
